e8e91c40f2d3ea3656d01f6b3e9bbb9d6ab3bbf9
Shayan Jamil <76084810+ShayanJamil519@users.noreply.github.com>
2024-03-03 11:59:00 +0500

    Fixes in  WasteManagement.sol

--- contracts/WasteManagement.sol
+++ contracts/WasteManagement.sol
@@ -1,83 +1,129 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 contract WasteManagementSystem {
-
     // Structs
     struct WasteCollectionEntry {
-        uint id;
+        uint256 id;
         address districtAdmin;
-        uint date; 
-        uint totalAmount; // Total amount of waste collected
+        uint256 date;
+        uint256 totalAmount; // Total amount of waste collected
         string area;
         string notes;
-       
-    }
-   
-    struct InputOutputEntry {
-        uint id;
-        uint inputDate; // UNIX timestamp for input
-        uint outputDate; // UNIX timestamp for output
-        uint quantityReceived;
-        uint recyclablePercentage;
-        string area;
-      
     }
 
-     struct LandfillEntry {
-        uint id;
-        uint date; // UNIX timestamp for entry
-        uint quantityDisposed;
+    struct InputOutputEntry {
+        uint256 id;
+        uint256 inputDate; // UNIX timestamp for input
+        uint256 outputDate; // UNIX timestamp for output
+        uint256 quantityReceived;
+        uint256 recyclablePercentage;
+        string area;
+    }
+
+    struct InputEntry {
+        uint256 id;
+        uint256 inputDate; // UNIX timestamp for input
+        uint256 quantityReceived;
+        string district;
+        string sourceSubdivision;
+        string area;
+    }
+
+    struct OutputEntry {
+        uint256 id;
+        uint256 outputDate; // UNIX timestamp for input
+        uint256 recyclablePercentage;
+        uint256 plasticPercentage;
+        uint256 glassPercentage;
+        uint256 metalloidsPercentage;
+        uint256 marketValue;
+    }
+
+    struct LandfillEntry {
+        uint256 id;
+        uint256 date; // UNIX timestamp for entry
+        uint256 quantityDisposed;
         string area;
         string landfillSite;
-        
     }
 
     struct WeeklyReport {
-        uint id;
+        uint256 id;
         address admin;
         string ipfsHash;
-        uint date; // UNIX timestamp for the report submission
+        uint256 date; // UNIX timestamp for the report submission
+        string reportType;
     }
 
-    enum UserRole { DistrictAdmin, RecyclableAdmin, LandfillAdmin, User }
-
+    enum UserRole {
+        DistrictAdmin,
+        RecyclableAdmin,
+        LandfillAdmin,
+        User
+    }
 
     // Variables
     address public owner;
-  
+
     // Reports Ids
-    uint private districtAdminReportId = 1;
-    uint private recyclablePointAdminReportId = 1;
-    uint private landfillAdminReportId = 1;
+    uint256 private weeklyReportId = 1;
 
     // Entries Ids
-    uint private wasteCollectionEntryId = 1;    // district admin
-    uint private inputOutputEntryId = 1;     // recycle point admin
-    uint private landfillEntryId = 1;       // landfill admin
+    uint256 private wasteCollectionEntryId = 1; // district admin
+    uint256 private inputEntryId = 1; // recycle point admin
+    uint256 private outputEntryId = 1; // recycle point admin
+    uint256 private landfillEntryId = 1; // landfill admin
 
-    
     // Entries Mappings
-    mapping(uint => WasteCollectionEntry) public wasteCollectionEntries;
-    mapping(uint => InputOutputEntry) public inputOutputEntries;
-    mapping(uint => LandfillEntry) public landfillEntries; 
+    mapping(uint256 => WasteCollectionEntry) public wasteCollectionEntries;
+    mapping(uint256 => InputEntry) public inputEntries;
+    mapping(uint256 => OutputEntry) public outputEntries;
+    mapping(uint256 => LandfillEntry) public landfillEntries;
 
     // Reports Mappings
-    mapping(uint => WeeklyReport) public districtAdminReports;
-    mapping(uint => WeeklyReport) public recyclablePointAdminReports;
-    mapping(uint => WeeklyReport) public landfillAdminReports;
+    mapping(uint256 => WeeklyReport) public weeklyReports;
 
     // Roles Mappings
     mapping(address => UserRole) public userRoles;
 
     // Events
-    event WasteCollectionRecorded(uint id, address districtAdmin, uint totalAmount, string area);
-    event InputOutputRecorded(uint id, uint quantityReceived, string area);
-    event LandfillEntryRecorded(uint id, uint quantityDisposed, string area, string landfillSite); 
-    
+    event WasteCollectionRecorded(
+        uint256 id,
+        address districtAdmin,
+        uint256 totalAmount,
+        string area
+    );
+    event InputEntryRecorded(
+        uint256 id,
+        uint256 date,
+        uint256 quantityReceived,
+        string _area
+    );
+    event OutputEntryRecorded(
+        uint256 id,
+        uint256 date,
+        uint256 recyclablePercentage,
+        uint256 marketValue
+    );
+    event LandfillEntryRecorded(
+        uint256 id,
+        uint256 quantityDisposed,
+        string area,
+        string landfillSite
+    );
+    event WeeklyReportAdded(
+        uint256 id,
+        address admin,
+        string ipfsHash,
+        string reportType
+    );
 
     modifier onlyRole(UserRole role) {
-        require(userRoles[msg.sender] == role, "Unauthorized action for user role.");
+        require(
+            userRoles[msg.sender] == role,
+            "Unauthorized action for user role."
+        );
         _;
     }
 
@@ -86,80 +132,233 @@ contract WasteManagementSystem {
         userRoles[owner] = UserRole.DistrictAdmin;
     }
 
-
     // District Admin
-    function recordWasteCollection(uint _date, uint _totalAmount, string memory _area, string memory _notes) public onlyRole(UserRole.DistrictAdmin) {
-        wasteCollectionEntries[wasteCollectionEntryId] = WasteCollectionEntry(wasteCollectionEntryId, msg.sender, _date, _totalAmount, _area, _notes);
-        emit WasteCollectionRecorded(wasteCollectionEntryId, msg.sender, _totalAmount, _area);
+    function recordWasteCollection(
+        uint256 _date,
+        uint256 _totalAmount,
+        string memory _area,
+        string memory _notes
+    ) public onlyRole(UserRole.DistrictAdmin) {
+        wasteCollectionEntries[wasteCollectionEntryId] = WasteCollectionEntry(
+            wasteCollectionEntryId,
+            msg.sender,
+            _date,
+            _totalAmount,
+            _area,
+            _notes
+        );
+        emit WasteCollectionRecorded(
+            wasteCollectionEntryId,
+            msg.sender,
+            _totalAmount,
+            _area
+        );
         wasteCollectionEntryId++;
     }
 
-      //   Recyclable Point Admin
-    function recordInputOutput(uint _inputDate, uint _outputDate, uint _quantityReceived, uint _recyclablePercentage, string memory _area) public onlyRole(UserRole.RecyclableAdmin) {
-        inputOutputEntries[inputOutputEntryId] = InputOutputEntry(inputOutputEntryId, _inputDate, _outputDate, _quantityReceived, _recyclablePercentage, _area);
-        emit InputOutputRecorded(inputOutputEntryId, _quantityReceived, _area);
-        inputOutputEntryId++;
+    // Recycle point admin
+    function recordInputEntry(
+        uint256 _inputDate,
+        uint256 _quantityReceived,
+        string memory _district,
+        string memory _sourceSubdivision,
+        string memory _area
+    ) public onlyRole(UserRole.RecyclableAdmin) {
+        inputEntries[inputEntryId] = InputEntry(
+            inputEntryId,
+            _inputDate,
+            _quantityReceived,
+            _district,
+            _sourceSubdivision,
+            _area
+        );
+        emit InputEntryRecorded(
+            inputEntryId,
+            _inputDate,
+            _quantityReceived,
+            _area
+        );
+        inputEntryId++;
     }
 
-      function recordLandfillEntry(uint _date, uint _quantityDisposed, string memory _area, string memory _landfillSite) public onlyRole(UserRole.LandfillAdmin) {
-        landfillEntries[landfillEntryId] = LandfillEntry(landfillEntryId, _date, _quantityDisposed, _area, _landfillSite);
-        emit LandfillEntryRecorded(landfillEntryId, _quantityDisposed, _area, _landfillSite);
+    function recordOutputEntry(
+        uint256 _outputDate,
+        uint256 recyclablePercentage,
+        uint256 _plasticPercentage,
+        uint256 _glassPercentage,
+        uint256 _metalloidsPercentage,
+        uint256 _marketValue
+    ) public onlyRole(UserRole.RecyclableAdmin) {
+        outputEntries[outputEntryId] = OutputEntry(
+            outputEntryId,
+            _outputDate,
+            recyclablePercentage,
+            _plasticPercentage,
+            _glassPercentage,
+            _metalloidsPercentage,
+            _marketValue
+        );
+        emit OutputEntryRecorded(
+            outputEntryId,
+            _outputDate,
+            recyclablePercentage,
+            _marketValue
+        );
+        outputEntryId++;
+    }
+
+    // Landfill admin
+    function recordLandfillEntry(
+        uint256 _date,
+        uint256 _quantityDisposed,
+        string memory _area,
+        string memory _landfillSite
+    ) public onlyRole(UserRole.LandfillAdmin) {
+        landfillEntries[landfillEntryId] = LandfillEntry(
+            landfillEntryId,
+            _date,
+            _quantityDisposed,
+            _area,
+            _landfillSite
+        );
+        emit LandfillEntryRecorded(
+            landfillEntryId,
+            _quantityDisposed,
+            _area,
+            _landfillSite
+        );
         landfillEntryId++;
     }
 
-    
- 
+    // Add weekly report function
+    function addWeeklyReport(uint _date, string memory _ipfsHash, string memory _reportType)
+        public
+    {
+        WeeklyReport memory newReport = WeeklyReport(
+            weeklyReportId,
+            msg.sender,
+            _ipfsHash,
+            _date,
+            _reportType
+        );
+        weeklyReports[weeklyReportId] = newReport;
+        emit WeeklyReportAdded(
+            weeklyReportId,
+            msg.sender,
+            _ipfsHash,
+            _reportType
+        );
+        weeklyReportId++;
+    }
+
     // Additional logic to assign roles to users
     function assignUserRole(address _user, UserRole _role) public {
-        require(msg.sender == owner, "Only the contract owner can assign roles.");
+        require(
+            msg.sender == owner,
+            "Only the contract owner can assign roles."
+        );
         userRoles[_user] = _role;
     }
 
     // Function to allow district admins to update waste collection records (if necessary)
-    function updateWasteCollectionEntry(uint _id, uint _totalAmount, string memory _notes) public onlyRole(UserRole.DistrictAdmin) {
-        require(_id < wasteCollectionEntryId, "Waste collection entry does not exist.");
+    function updateWasteCollectionEntry(
+        uint256 _id,
+        uint256 _totalAmount,
+        string memory _notes
+    ) public onlyRole(UserRole.DistrictAdmin) {
+        require(
+            _id < wasteCollectionEntryId,
+            "Waste collection entry does not exist."
+        );
         WasteCollectionEntry storage entry = wasteCollectionEntries[_id];
         entry.totalAmount = _totalAmount;
         entry.notes = _notes;
-        emit WasteCollectionRecorded(_id, entry.districtAdmin, _totalAmount, entry.area);
+        emit WasteCollectionRecorded(
+            _id,
+            entry.districtAdmin,
+            _totalAmount,
+            entry.area
+        );
     }
 
-
-    
     // Function to get a waste collection entry by its ID
-    function getWasteCollectionEntry(uint _id) public view returns (WasteCollectionEntry memory) {
-        require(_id < wasteCollectionEntryId, "Waste collection entry does not exist.");
+    function getWasteCollectionEntry(uint256 _id)
+        public
+        view
+        returns (WasteCollectionEntry memory)
+    {
+        require(
+            _id < wasteCollectionEntryId,
+            "Waste collection entry does not exist."
+        );
         return wasteCollectionEntries[_id];
     }
 
+    // Function to get an input-output entry by its ID
+    function getInputEntry(uint256 _id)
+        public
+        view
+        returns (InputEntry memory)
+    {
+        require(_id < inputEntryId, "Input entry does not exist.");
+        return inputEntries[_id];
+    }
 
-      // Function to get an input-output entry by its ID
-    function getInputOutputEntry(uint _id) public view returns (InputOutputEntry memory) {
-        require(_id < inputOutputEntryId, "Input-output entry does not exist.");
-        return inputOutputEntries[_id];
+    function getOutputEntry(uint256 _id)
+        public
+        view
+        returns (OutputEntry memory)
+    {
+        require(_id < outputEntryId, "Output entry does not exist.");
+        return outputEntries[_id];
     }
 
     // Function to get an landfill entry by its ID
-     function getLandfillEntry(uint _id) public view returns (LandfillEntry memory) {
+    function getLandfillEntry(uint256 _id)
+        public
+        view
+        returns (LandfillEntry memory)
+    {
         require(_id < landfillEntryId, "Landfill entry does not exist.");
         return landfillEntries[_id];
     }
 
-
-        // Getter functions for the reports
-    function getDistrictAdminReport(uint _id) public view returns (WeeklyReport memory) {
-        require(_id < districtAdminReportId, "District Admin report does not exist.");
-        return districtAdminReports[_id];
+    // Getter functions for the reports
+    function getWeeklyReport(uint256 _id)
+        public
+        view
+        returns (WeeklyReport memory)
+    {
+        require(_id < weeklyReportId, "Weekly report does not exist.");
+        return weeklyReports[_id];
     }
 
-    function getRecyclablePointAdminReport(uint _id) public view returns (WeeklyReport memory) {
-        require(_id < recyclablePointAdminReportId, "Recyclable Point Admin report does not exist.");
-        return recyclablePointAdminReports[_id];
+    // Getter function to get all reports of a specific type
+    function getAllReportsByType(string memory _reportType)
+        public
+        view
+        returns (WeeklyReport[] memory)
+    {
+        uint256 count = 0;
+        for (uint256 i = 1; i < weeklyReportId; i++) {
+            if (
+                keccak256(bytes(weeklyReports[i].reportType)) ==
+                keccak256(bytes(_reportType))
+            ) {
+                count++;
+            }
+        }
+        WeeklyReport[] memory reports = new WeeklyReport[](count);
+        uint256 index = 0;
+        for (uint256 i = 1; i < weeklyReportId; i++) {
+            if (
+                keccak256(bytes(weeklyReports[i].reportType)) ==
+                keccak256(bytes(_reportType))
+            ) {
+                reports[index] = weeklyReports[i];
+                index++;
+            }
+        }
+        return reports;
     }
-
-    function getLandfillAdminReport(uint _id) public view returns (WeeklyReport memory) {
-        require(_id < landfillAdminReportId, "Landfill Admin report does not exist.");
-        return landfillAdminReports[_id];
-    }
-
 }
